bbfaad553080db340da6fd0e59f02ec6e4f43ce7
Alex Pomazan <multdirectx@gmail.com>
2020-01-22 16:38:26 +0200

    added prettier

--- graphql/schema/machine.js
+++ graphql/schema/machine.js
@@ -1,7 +1,7 @@
 import { gql } from 'apollo-server-express'
 
 export default gql`
- type Query {
-     test: Int
- }
+  type Query {
+    test: Int
+  }
 `
--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import { ApolloServer } from 'apollo-server-express'
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas'
 import path from 'path'
 
-const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './graphql/resolvers')))
-const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './graphql/schema')), { all: true })
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, './graphql/resolvers'))
+)
+const typeDefs = mergeTypes(
+  fileLoader(path.join(__dirname, './graphql/schema')),
+  { all: true }
+)
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,4 +22,6 @@ server.applyMiddleware({ app })
 
 const port = 4000
 
-app.listen({ port }, () => console.log(`🚀 Server ready at http://localhost:${port}/graphql`))
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
+)
